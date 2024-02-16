6434d5ee9f3b8fdea9dc251506d022ecba1ce6a0
Peter2024H <159722619+Peter2024H@users.noreply.github.com>
2024-02-16 06:35:47 +0000

    spotify update and fix jpg

--- src/js/Home.js
+++ src/js/Home.js
@@ -6,31 +6,137 @@ import {Slideshow} from './helper/slideShow';
 import { HashLink } from 'react-router-hash-link';
 import Footer from "./helper/Footer";
 import {Top} from "./helper/Top";
+import React, { useEffect, useState } from "react";
 import HorizontalScroll from "./helper/horizontalScroll";
+import axios from "axios";
+// const PLAYLISTS_ENDPOINT = "https://api.spotify.com/v1/me/playlists/";
+// const PLAYLISTS_ENDPOINT = "https://api.spotify.com/v1/me/top/artists/";
+const PLAYLISTS_ENDPOINT = "https://api.spotify.com/v1/me/top/tracks?time_range=long_term";
+// const PLAYLISTS_ENDPOINT = "https://api.spotify.com/v1/me/player/currently-playing/";
+
+
+
+
+
 export function Home() { 
+    const [token, setToken] = useState("no");
+    const [data, setData] = useState({});
+    /* 
+    http://localhost:3000/webapp#access_token=ABCqxL4Y&token_type=Bearer&expires_in=3600
+    */
+
+    const getReturnedParamsFromSpotifyAuth = (hash) => {
+        const stringAfterHashtag = hash.substring(1);
+        const paramsInUrl = stringAfterHashtag.split("&");
+        const paramsSplitUp = paramsInUrl.reduce((accumulater, currentValue) => {
+        console.log(currentValue);
+        const [key, value] = currentValue.split("=");
+        accumulater[key] = value;
+        return accumulater;
+        }, {});
+    
+        return paramsSplitUp;
+    };
+    
+
+    useEffect(() => {
+      if (window.location.hash) {
+        const { access_token, expires_in, token_type } =
+        getReturnedParamsFromSpotifyAuth(window.location.hash);
+        localStorage.clear();
+        localStorage.setItem("accessToken", access_token);
+        localStorage.setItem("tokenType", token_type);
+        localStorage.setItem("expiresIn", expires_in);
+ 
+        console.log(localStorage.getItem("accessToken"));
+    
+        if(data.length===0){
+          console.log(token_type);
+          console.log(expires_in);
+          alert("Login Spotify Success!");
+        }
+        if(localStorage.getItem("accessToken")){
+            setToken(localStorage.getItem("accessToken"));
+        }
+      }
+    });
+
+    const handleGetPlaylists = () => {
+        // console.log(token);
+        axios
+          .get(PLAYLISTS_ENDPOINT, {
+            headers: {
+              Authorization:`Bearer ${token}`,
+            },
+          })
+          .then((response) => {
+            console.log(response.data)
+            // playlist
+            // console.log(response.data["items"][0]["images"][0]["url"])
+            // top tracks
+            // console.log(response.data["items"].length)
+            // console.log(response.data["items"][0])
+            // console.log(response.data["items"][0]["artists"][0]["name"])
+            // console.log(response.data["items"][0]["name"])
+            setData(response.data["items"]); 
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      };
+    
     return (
         <>
             <center>
             <Top />
-            <br/>
-                {/* <img src={movie} alt="movie" height="80" ></img>
+
+            {/* <img src={movie} alt="movie" height="80" ></img>
                 <img src={book} alt="book" height="80" ></img>
                 <img src={music} alt="music" height="80" ></img> */}
-            
-            <Slideshow />
+        <div className="two-column-container">
+          <div className="column">
+          
+            <b><i>Your</i> Top Tracks</b>  
+             
+            <div className='recommendation-box'>
+            { data.length>0? (data.map((item, index) => (
+              <div key={index} className='recommendation-item'>
+              {/* Check if "artists" array exists and has at least one item */}
+              {item.artists && item.artists.length > 0 && 
+              (<div className="song-info">
+                {item.album.images.length>0 &&(
+               <img src={item.album.images[0].url} alt={item.name} className='album-image' height="15"></img>)}
+              {/* <p className='song-name'>{item.name}</p> --- <p className='artist-name'><i>{item.artists[0].name}</i></p> */}
+                <b>{item.name}</b> --- <i>{item.artists[0].name}</i>
+              </div>
+               )}
+              </div>
+            ))) : 
+            <p className="no-data">No data available before login and import.</p>
+            }
+            </div>
+            <button onClick={handleGetPlaylists}>Import Spotify Data (Login)</button> <br></br>
+     
+        </div>
 
+        <div className="column">
+            <Slideshow />
+            <audio src={music_mp3} autoplay="autoplay" controls loop="loop" >Example audio</audio>
+
+          </div>
+        </div>
+
+          
             <HorizontalScroll />
 
-            <h2>Example audio</h2>
-            <audio src={music_mp3} autoplay="autoplay" controls loop="loop"></audio>
-
-            <h2>Try example subpage</h2> 
-            <HashLink to="/example"><i>Example Subpage</i></HashLink>
+          
+            {/* <h2>Try example subpage</h2> 
+            <HashLink to="/example"><i>Example Subpage</i></HashLink> */}
            
-            <h2>Developer Notice Board</h2> 
+            {/* <h2>Developer Notice Board</h2> 
             Change me from src/index.js <br/>
             Other subpages implemented at src/js/ <br/>
-            Reference: <a href="https://reactjs.org">Learn React</a> <br/>
+            Reference: <a href="https://reactjs.org">Learn React</a> <br/> */}
             </center>
 
             <Footer />
---
+++ src/js/Setup.js
@@ -0,0 +1,93 @@
+import React, { useEffect, useRef, useState } from "react";
+import { useLocation } from "react-router-dom";
+import {Top} from "./helper/Top";
+import Footer from "./helper/Footer";
+import email from '../asset/login/email.png';
+import password from '../asset/login/password.png';
+import person from '../asset/login/person.png';
+export function Setup(props)
+{
+    const [action,setAction] = useState("Login");
+    const [val, setVal] = useState("Name");
+    const handleChange = (event) => {
+        // 👇 Get input value from "event"
+        setVal(event.target.value);
+    };
+    const clickHandler = ()=>{
+        alert("Welcome! User profile updated successfully~");
+        window.location = `/`;
+    }
+    const clickHandlerToHome = ()=>{
+        window.location = `/`;
+    }
+
+    const CLIENT_ID = "296e5dd18ce041a48bc1f416312b3013";
+    const SPOTIFY_AUTHORIZE_ENDPOINT="https://accounts.spotify.com/authorize";
+    // const REDIRECT_URL = "http://localhost:3000";
+    const REDIRECT_URL = "https://peter2024h.github.io/";
+
+    const SPACE_DELIMITER = "%20";
+    const SCOPES = ["user-read-currently-playing","user-read-playback-state","user-top-read", "user-read-recently-played"];
+    const SCOPES_URL = SCOPES.join(SPACE_DELIMITER);
+    const handleSpotify = () => {
+        window.location = `${SPOTIFY_AUTHORIZE_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL}&scope=${SCOPES_URL}&response_type=token&show_dialog=true`;
+    }
+
+    return(
+        <>
+        <Top />
+        <center>
+            <br/>
+            <div className='container'>
+                <div className='header'>
+                    <div className="text">{action}</div>
+                    <div className="underline"></div>
+                </div>
+                {/* Option to choose from  */}
+                <div className="submit-container">
+                    <div className={action==='Sign Up'?"submit gray":"submit"}
+                    onClick={()=>{setAction("Login")}}>Login</div> 
+                    <div className={action==='Sign Up'?"submit gray":"submit"} onClick={handleSpotify}> 
+                        Spotify 
+                    </div>    
+                    <div className={action==='Login'?"submit gray":"submit"}
+                    onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
+                </div>
+                {/* IO */}
+                <div className="forgot-password">Hello {val}! </div>
+                <div className="inputs">
+                    <div className="input">
+                        <img src={person} alt="person"></img>
+                        <input type="text" placeholder="Name" onChange={handleChange}></input>
+                    </div>
+                    <div className="input">
+                        <img src={email} alt="email"></img>
+                        <input type="email" placeholder="Email"></input>
+                    </div>
+                    <div className="input">
+                        <img src={password} alt="password"></img>
+                        <input type="password" placeholder="Password"></input>
+                    </div>
+                </div>
+                {/* Submit */}
+                <div className="home-container">
+                    <div className={"home"} onClick={clickHandler}> 
+                        Submit
+                    </div>
+                    <div className={"home"} onClick={clickHandlerToHome}> 
+                        🏠HOME
+                    </div>
+                </div>
+                <div className="forgot-password">Oh! Forget Password? </div>
+
+            </div>
+            {/* <AuthProvider authType = {'cookie'}
+                          authName = {'_auth'}
+            ></AuthProvider> */}
+
+           
+        </center>
+        <Footer />
+        </>
+    )
+}
